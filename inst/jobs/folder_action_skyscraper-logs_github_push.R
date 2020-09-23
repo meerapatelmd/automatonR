974db9a3b119374e1420754382804fe413004a94
Meera <patelmeeray@gmail.com>
2020-09-23 00:05:31 -0400

    update skyscraper-log job

--- inst/jobs/folder_action_skyscraper-logs_github_push.R
+++ inst/jobs/folder_action_skyscraper-logs_github_push.R
@@ -3,8 +3,7 @@ library(glitter)
 
 
 repo_path <- path.expand("~/Desktop/skyscraper-logs")
-
+glitter::pull(path_to_local_repo = repo_path)
 glitter::add_commit_all(commit_message = "automatic push",
                         path_to_local_repo = repo_path)
-
 glitter::push(path_to_local_repo = repo_path)
