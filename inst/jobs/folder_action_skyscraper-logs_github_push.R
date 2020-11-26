f07de1ae3a5589dca4b2605da0b3947799c0083b
Meera Patel <patelmeeray@gmail.com>
2020-11-26 12:28:34 -0500

    convert to project

--- inst/jobs/folder_action_skyscraper-logs_github_push.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(glitter)
-
-
-repo_path <- path.expand("~/Desktop/skyscraper-logs")
-glitter::pull(path_to_local_repo = repo_path)
-glitter::add_commit_all(commit_message = "automatic push",
-                        path_to_local_repo = repo_path)
-glitter::push(path_to_local_repo = repo_path)
